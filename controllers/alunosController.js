81f39ebe96ebf7d4ef42872e0ebf6f7c89c04046
Henrique Barros <henriquesbarroshs@gmail.com>
2021-04-19 16:57:46 -0300

    CRUD alunos e professores

--- controllers/alunosController.js
+++ controllers/alunosController.js
@@ -1,29 +1,58 @@
-const { Aluno } = require('../models')
+const { Aluno, alunoDisciplina, Disciplina } = require('../models')
 
 const alunosController = {
+    index: async (req, res) => {
+        const studentFound = await Aluno.findAll()
+        return res.json(studentFound)
+    },
     create: async (req, res) => {
-        const { nome, cpf, img_perfil } = req.body
+        const { modulos_id, nome, cpf, img_perfil } = req.body
         const novoAluno = await Aluno.create({
             nome,
             cpf,
             img_perfil
         })
 
-        // const disciplinas = await Disciplina.findAll({
-        //     where: {
-        //         modulos_id
-        //     }
-        // })
+        const disciplinas = await Disciplina.findAll({
+            where: {
+                modulos_id
+            }
+        })
 
-        // const alunoDiscPromise = disciplinas.map(disciplina => {
-        //     alunoDisciplina.create({
-        //         alunos_id: novoAluno.id,
-        //         disciplinas_id: disciplina.id
-        //     })
-        // })
+        const alunoDiscPromise = disciplinas.map(disciplina => {
+            alunoDisciplina.create({
+                alunos_id: novoAluno.id,
+                disciplinas_id: disciplina.id
+            })
+        })
 
-        // const promisesFinalized = await Promise.all(alunoDiscPromise)
-        return res.json(novoAluno)
+        const promisesFinalized = await Promise.all(alunoDiscPromise)
+        return res.json(promisesFinalized)
+    },
+    update: async (req, res) => {
+        const { id } = req.params
+        const { nome } = req.body
+
+        const attAluno = await Aluno.update({
+            nome
+        }, 
+        {
+            where: {
+                id
+            }
+        })
+
+        return res.json(attAluno)
+    },
+    delete: async (req, res) => {
+        const { id } = req.params
+        const delAaluno = Aluno.destroy({
+            where: {
+                id
+            }
+        })
+
+        return res.json(delAaluno)
     }
 }
 
