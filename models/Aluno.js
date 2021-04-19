1a4b0a3b6dea9e5e183df8b162d5a8e181e65247
joao-sotero <vitor99jjunior@gmail.com>
2021-04-19 14:49:00 -0300

    controller e models alterados. add middlewares

--- models/Aluno.js
+++ models/Aluno.js
@@ -11,6 +11,11 @@ module.exports = (Sequelize, DataTypes) => {
         timestamps: false
     }
     )
+
+     // To create a One - To - One relationship, the hasOne and belongsTo associations are used together;
+    // To create a One - To - Many relationship, the hasMany and belongsTo associations are used together;
+    // To create a Many - To - Many relationship, two belongsToMany calls are used together
+
     Aluno.associate = (models) => {
         Aluno.hasMany(models.alunoDisciplina,{ as: "notas", foreignKey: "alunos_id" })
         
