287bf2d9e126933c44fa3462f8846abdbfc0ad6b
joao-sotero <vitor99jjunior@gmail.com>
2021-04-22 09:17:23 -0300

    possivel solução do filtro de alunos

--- routes/professor.js
+++ routes/professor.js
@@ -5,7 +5,10 @@ const professoresController = require('../controllers/professoresController')
 //aluno
 router.get('/entrar', professoresController.login)
 // VERIFICACR O PQ /:id não funciona
-router.get('/professor/:id', professoresController.show)
+// router.get('/professor/:id', professoresController.show)
+
+//verificar se assim funciona
+router.get('/:id', professoresController.show)
 router.get('/listagem', professoresController.listagemAlunos)
 router.get('/notas/:id', professoresController.notas)
 router.put('/notas/:idAluno/:idDisciplina', professoresController.putNotas)
