123b99f2e031071e73d47b0dc6f3118b77e0376c
Henrique Barros <henriquesbarroshs@gmail.com>
2021-04-22 17:41:25 -0300

    Update

--- routes/professor.js
+++ routes/professor.js
@@ -4,10 +4,7 @@ const professoresController = require('../controllers/professoresController')
 
 //aluno
 router.get('/entrar', professoresController.login)
-// VERIFICACR O PQ /:id não funciona
-// router.get('/professor/:id', professoresController.show)
-
-//verificar se assim funciona
+router.get('/professor/:id', professoresController.show)
 router.get('/:id', professoresController.show)
 router.get('/listagem', professoresController.listagemAlunos)
 router.get('/notas/:id', professoresController.notas)
