46124e5c2ed38ffc25941059563bbf37e80a90a5
Henrique Barros <henriquesbarroshs@gmail.com>
2021-05-06 14:16:53 -0300

    Final

--- middlewares/validaCampoBrancoAluno.js
+++ middlewares/validaCampoBrancoAluno.js
@@ -2,9 +2,9 @@ module.exports = (req, res, next) =>{
     let { cpf, nome_aluno, modulo_id } = req.body;
     
     if ( !nome_aluno || !cpf|| !modulo_id) {
-        res.render('error', { error: `CPF ja cadastrado!` })
+        res.render('error', { error: `Preencha todos os campos!` })
         // res.status(400).json({ erro: "campo em branco" });
     } else {
-            next();
+        next();
     }
 }
